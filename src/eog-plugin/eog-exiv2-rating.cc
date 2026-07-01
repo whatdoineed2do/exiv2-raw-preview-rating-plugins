2b4c979408856adbc0ed9e10bdb8cc47ca8983a7
whatdoineed2do/Ray <whatdoineed2do@nospam.gmail.com>
2026-07-01 22:01:58 +0100

    eog: accel keybindings 0R2345 and T (can't use 1 as its used for 100% size

--- src/eog-plugin/eog-exiv2-rating.cc
+++ src/eog-plugin/eog-exiv2-rating.cc
@@ -80,10 +80,32 @@ _exiv2_rating_setunset(GSimpleAction *simple, GVariant *parameter, gpointer user
 }
 
 static void
-_exiv2_rating_set_cb(GSimpleAction *simple, GVariant      *parameter, gpointer       user_data)
+_exiv2_rating_set_cb(GSimpleAction *simple, GVariant *parameter, gpointer user_data)
 {
-    _exiv2_rating_setunset(simple, parameter, user_data, true);
+    EogExiv2RatingPlugin *plugin = EOG_EXIV_XMP_RATING_PLUGIN (user_data);
+
+    const bool b = [&parameter, &plugin]() {
+        // If no parameter is passed, default to the toggle behavior
+        if (parameter == NULL) {
+            return plugin->exifproxy->fliprating();
+        }
+
+        // Extract the integer rating (0-5)
+        gint32 rating = g_variant_get_int32(parameter);
+        if (rating > 5 || rating < 0) rating = 0;
+
+        return plugin->exifproxy->rate(static_cast<unsigned short>(rating));
+    }();
+
+    _upd_statusbar_exif(GTK_STATUSBAR(plugin->statusbar_exif),
+                        b ? plugin->exifproxy->rating() : "-/-");
 }
+
+// Define the actions array right below the callback
+static const GActionEntry plugin_actions[] = {
+    { EOG_EXIV_XMP_RATING_PLUGIN_ACTION, _exiv2_rating_set_cb, NULL, NULL, NULL, {0} }, // Toggle ("R")
+    { "exiv2-set-rating",                _exiv2_rating_set_cb, "i",  NULL, NULL, {0} }  // Parameterized (0-5, r)
+};
 
 static void
 _exiv2_rating_unset_cb(GSimpleAction *simple, GVariant *parameter, gpointer user_data)
@@ -201,6 +223,12 @@ eog_exiv2_ratings_plugin_update_action_state (EogExiv2RatingPlugin *plugin, EogT
     action = g_action_map_lookup_action (G_ACTION_MAP (plugin->window),
                                          EOG_EXIV_XMP_RATING_PLUGIN_ACTION);
     g_simple_action_set_enabled (G_SIMPLE_ACTION (action), enable);
+
+    GAction *rating_action = g_action_map_lookup_action (G_ACTION_MAP (plugin->window),
+                                                         "exiv2-set-rating");
+    if (rating_action) {
+        g_simple_action_set_enabled (G_SIMPLE_ACTION (rating_action), enable);
+    }
 }
 
 static void
@@ -216,11 +244,8 @@ _selection_changed_cb (EogThumbView *thumbview, gpointer user_data)
 static void
 eog_exiv2_ratings_plugin_activate (EogWindowActivatable *activatable)
 {
-    const gchar * const accel_keys[] = { "R", NULL };
     EogExiv2RatingPlugin *plugin = EOG_EXIV_XMP_RATING_PLUGIN (activatable);
     GMenu *model;
-    GSimpleAction *action;
-
 
     GtkWidget *statusbar = eog_window_get_statusbar(plugin->window);
     plugin->statusbar_exif = gtk_statusbar_new();
@@ -233,40 +258,49 @@ eog_exiv2_ratings_plugin_activate (EogWindowActivatable *activatable)
     gtk_widget_set_size_request (plugin->statusbar_exif, natw, nath);
     gtk_box_pack_end (GTK_BOX(statusbar), plugin->statusbar_exif, FALSE, FALSE, 0);
 
-
-#ifdef EOG_PLUGIN_DEBUG 
+#ifdef EOG_PLUGIN_DEBUG
     eog_debug (DEBUG_PLUGINS);
 #endif
 
-    model= eog_window_get_gear_menu_section (plugin->window,
-                                             "plugins-section");
-
+    model = eog_window_get_gear_menu_section (plugin->window, "plugins-section");
     g_return_if_fail (G_IS_MENU (model));
 
-    /* Setup and inject action */
-    action = g_simple_action_new (EOG_EXIV_XMP_RATING_PLUGIN_ACTION, NULL);
-#ifndef MULTI_CB
-    g_signal_connect(action, "activate", G_CALLBACK (_exiv2_rating_set_cb), plugin);
-    g_action_map_add_action (G_ACTION_MAP (plugin->window), G_ACTION (action));
-#else
-    g_action_map_add_action_entries(G_ACTION_MAP(plugin->window),
-                                    actions, G_N_ELEMENTS(actions), plugin->window);
-#endif
+    /* 1. Register Action Map Entries */
+    g_action_map_add_action_entries (G_ACTION_MAP (plugin->window),
+                                     plugin_actions,
+                                     G_N_ELEMENTS (plugin_actions),
+                                     plugin);
 
-    g_object_unref (action);
+    /* 2. Setup Acceleration Keys (Local variables to satisfy C++) */
+    const gchar *accel_t[] = { "T", NULL };
+    const gchar *accel_0[] = { "0", NULL };
+    const gchar *accel_1[] = { "r", NULL }; // Map 'r' to rating value 1
+    const gchar *accel_2[] = { "2", NULL };
+    const gchar *accel_3[] = { "3", NULL };
+    const gchar *accel_4[] = { "4", NULL };
+    const gchar *accel_5[] = { "5", NULL };
+
+    GtkApplication *app = GTK_APPLICATION (EOG_APP);
+    if (app != NULL) {
+        // Toggle key
+        gtk_application_set_accels_for_action (app, "win." EOG_EXIV_XMP_RATING_PLUGIN_ACTION, accel_t);
+
+        // Parameterized key mapping (using 'int32' formatting parsing strings)
+        gtk_application_set_accels_for_action (app, "win.exiv2-set-rating(0)", accel_0);
+        gtk_application_set_accels_for_action (app, "win.exiv2-set-rating(1)", accel_1);
+        gtk_application_set_accels_for_action (app, "win.exiv2-set-rating(2)", accel_2);
+        gtk_application_set_accels_for_action (app, "win.exiv2-set-rating(3)", accel_3);
+        gtk_application_set_accels_for_action (app, "win.exiv2-set-rating(4)", accel_4);
+        gtk_application_set_accels_for_action (app, "win.exiv2-set-rating(5)", accel_5);
+    }
 
     g_signal_connect (G_OBJECT (eog_window_get_thumb_view (plugin->window)),
                       "selection-changed",
                       G_CALLBACK (_selection_changed_cb),
                       plugin);
     eog_exiv2_ratings_plugin_update_action_state (plugin, EOG_THUMB_VIEW (eog_window_get_thumb_view (plugin->window)));
-
-
-    /* Define accelerator keys */
-    gtk_application_set_accels_for_action (GTK_APPLICATION (EOG_APP),
-                                           "win." EOG_EXIV_XMP_RATING_PLUGIN_ACTION,
-                                           accel_keys);
 }
+
 
 static void
 eog_exiv2_ratings_plugin_deactivate (EogWindowActivatable *activatable)
@@ -278,19 +312,18 @@ eog_exiv2_ratings_plugin_deactivate (EogWindowActivatable *activatable)
     eog_debug (DEBUG_PLUGINS);
 #endif
 
-    /* Unset accelerator */
-    gtk_application_set_accels_for_action(GTK_APPLICATION (EOG_APP),
-                                          "win." EOG_EXIV_XMP_RATING_PLUGIN_ACTION,
-                                          empty_accels);
+    GtkApplication *app = GTK_APPLICATION (EOG_APP);
+    if (app != NULL) {
+        gtk_application_set_accels_for_action (app, "win." EOG_EXIV_XMP_RATING_PLUGIN_ACTION, empty_accels);
+        gtk_application_set_accels_for_action (app, "win.exiv2-set-rating", empty_accels);
+    }
 
-    /* Disconnect selection-changed handler as the thumbview would
-     * otherwise still cause callbacks during its own disposal */
     g_signal_handlers_disconnect_by_func (eog_window_get_thumb_view (plugin->window),
                                           (gpointer)_selection_changed_cb, plugin);
 
-    /* Finally remove action */
-    g_action_map_remove_action (G_ACTION_MAP (plugin->window),
-                                EOG_EXIV_XMP_RATING_PLUGIN_ACTION);
+    for (guint i = 0; i < G_N_ELEMENTS (plugin_actions); i++) {
+        g_action_map_remove_action (G_ACTION_MAP (plugin->window), plugin_actions[i].name);
+    }
 
     GtkWidget *statusbar = eog_window_get_statusbar (plugin->window);
     gtk_container_remove(GTK_CONTAINER (statusbar), plugin->statusbar_exif);
